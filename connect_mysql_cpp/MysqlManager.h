10ee50f877b1a9137de8371c909c58def567711e
chenmeng01 <chenmeng01@weidian.com>
2017-04-10 13:15:09 +0800

    add connect mysql cpp

---
+++ connect_mysql_cpp/MysqlManager.h
@@ -0,0 +1,60 @@
+#ifndef MYSQLMANAGER_H_    
+#define MYSQLMANAGER_H_   
+
+#include "../Common/CheckStringTools.h"   
+
+#include <mysql.h>   
+
+#include <string>    
+#include <iostream>    
+#include <vector>   
+
+#include <string.h>   
+
+using namespace std;   
+
+class MySQLManager    
+{    
+public:    
+        /*    
+         * Init MySQL    
+         * @param hosts:         Host IP address    
+         * @param userName:        Login UserName    
+         * @param password:        Login Password    
+         * @param dbName:        Database Name    
+         * @param port:                Host listen port number    
+         */    
+        MySQLManager(std::string hosts, std::string userName, std::string password, std::string dbName, unsigned int port);    
+        ~MySQLManager();    
+        void initConnection();    
+        /*    
+         * Making query from database    
+         * @param mysql:        MySQL Object    
+         * @param sql:                Running SQL command    
+         */    
+        bool runSQLCommand(std::string sql);    
+        /**    
+         * Destroy MySQL object    
+         * @param mysql                MySQL object    
+         */    
+        void destroyConnection();    
+        bool getConnectionStatus();    
+        vector< vector<string> > getResult();    
+protected:    
+        void setUserName(std::string userName);    
+        void setHosts(std::string hosts);    
+        void setPassword(std::string password);    
+        void setDBName(std::string dbName);    
+        void setPort(unsigned int port);    
+private:    
+        bool IsConnected;    
+        vector< vector<string> > resultList;    
+        MYSQL mySQLClient;    
+        unsigned int DEFAULTPORT;    
+        char * HOSTS;    
+        char * USERNAME;    
+        char * PASSWORD;    
+        char * DBNAME;    
+};   
+
+#endif /* MYSQLMANAGER_H_ */
